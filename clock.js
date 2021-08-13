c581f6552dbf5f5bacb08a08e7592917b669f000
Vitya23 <71597503+Vitya23@users.noreply.github.com>
2021-08-14 01:16:08 +0300

    he

---
+++ clock.js
@@ -0,0 +1,29 @@
+(function() {
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+    let launch = 'Sep 1,2021 00:00:00',
+        countDown = new Date(launch).getTime(),
+        x = setInterval(function() {
+
+            let now = new Date().getTime(),
+                distance = countDown - now;
+
+            document.getElementById("days").innerText =
+                Math.floor(distance / (day)),
+                document.getElementById("hours").innerText =
+                Math.floor((distance % (day)) / (hour)),
+                document.getElementById("minutes").innerText =
+                Math.floor((distance % (hour)) / (minute)),
+                document.getElementById("seconds").innerText =
+                Math.floor((distance % (minute)) / second);
+
+            if (distance < 0) {
+
+
+                clearInterval(x);
+            }
+            //seconds
+        }, 0)
+}());
